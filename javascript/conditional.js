ce4341e134bde4904b07b6ec7052023d855b6501
gideonkings <gsimon931@gmail.com>
2021-09-06 21:37:14 +0100

    learning html, css and javascript

---
+++ javascript/conditional.js
@@ -0,0 +1,12 @@
+//conditional
+const name = "gideon";
+
+//syntax
+if (name === "gideon") {
+    let person = {
+        
+    }
+    
+}    else {
+    console.log("false", false);
+    }
---
+++ javascript/sec-day.js
@@ -0,0 +1,14 @@
+// data types in javascript
+
+
+
+// string
+let string = "hello world";
+
+// boolean
+let trueboolean = true;
+let falseboolean = false;
+nvm 
+console.log("string", string);
+
+let object = 
